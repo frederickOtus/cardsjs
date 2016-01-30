90b620607d610bbc7965069b5d187d93b57e31d7
Si Gustafson-Zook <sig@haikulearning.com>
2016-01-30 16:19:50 -0500

    Moved js from html to js client.js

--- games/eugenics/static/client.js
+++ games/eugenics/static/client.js
@@ -16,3 +16,64 @@ socket.on('choose card', function(m){
     console.log('Need to choose a card');
 });
 
+// Unlimited Card Works
+
+var selected_card = "";
+
+function onCardSelect(clicked_id)
+{
+  console.log(clicked_id);
+  selected_card = clicked_id;
+
+  var cards = ["1", "2", "3", "4"];
+
+  for (i = 0; i < cards.length; i++)
+    {
+      $("#" + cards[i]).removeClass("selected");
+    }
+
+  $("#" + clicked_id).addClass("selected");
+}
+
+$(".card").on("click", onCardSelect(this.id));
+
+function beast()
+{
+	if(!selected_card){
+		console.log("no card selected");
+		return "nope";
+	}
+	window.alert([selected_card, "beast"]);
+}
+
+function breed()
+{
+	if(!selected_card){
+		console.log("no card selected");
+		return "nope";
+	}
+	window.alert([selected_card, "breed"]);
+}
+
+function quest()
+{
+	if(!selected_card){
+		console.log("no card selected");
+		return "nope";
+	}
+	window.alert([selected_card, "quest"]);
+}
+
+function attack()
+{
+	if(!selected_card){
+		console.log("no card selected");
+		return "nope";
+	}
+	window.alert([selected_card, "attack"]);
+}
+
+$(".beast").on("click", beast);
+$(".breed").on("click", breed);
+$(".quest").on("click", quest);
+$(".attack").on("click", attack);
